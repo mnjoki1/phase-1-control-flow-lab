ee60509a0679fe51a19fc50d115e552d0a3d9756
mitchelle.js <mitchellenjoki7@gmail.com>
2023-07-11 23:03:04 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+
+  if (feet <= 400) {
+    return "This one is on me"
+  } 
+else if (feet > 2000 && feet < 2500) {
+    return ( "i will gladly take your 30 bucks") 
+  }
+    else {
+      return ("No can do ")
+    }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+ return city === 'NYC' ? 'ok sounds good' : 'no go' 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
